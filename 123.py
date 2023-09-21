db64096b8e8f7c087bfbf124183ce5969aacce84
Hemanths0 <145091410+Hemanths0@users.noreply.github.com>
2023-09-21 00:25:28 -0700

    Add files via upload

---
+++ 123.py
@@ -0,0 +1,15 @@
+def encrypt(text,s):
+  result=""
+  for i in range(len(text)):
+           char=text[i]
+            
+           if(char.isupper()):
+                   result+=char((ord(char)+s-65)%26+65)
+           else:
+                   result+=char((ord(char)+s-97)%26+97)
+  return  result
+text="ATTACKTONCE"
+s=4
+print("Text:"+text)
+print("Shift:"+ str(s))
+print("Cipher:"+ encrypt(text,s))
